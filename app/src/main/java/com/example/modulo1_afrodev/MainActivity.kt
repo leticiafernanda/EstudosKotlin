847f60091eac31fe36148874c362d106c8a22608
leticiafernanda <fernandaleehticia@gmail.com>
2020-11-09 18:51:19 -0300

    start project

---
+++ app/src/main/java/com/example/modulo1_afrodev/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.modulo1_afrodev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
